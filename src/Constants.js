c685f54057f10df675010cd17f1d89ed8b8d35b2
TanB2015010 <tanb2015010@student.ctu.edu.vn>
2023-11-05 20:43:00 +0700

    fix

--- src/Constants.js
+++ src/Constants.js
@@ -8,6 +8,12 @@ export const backgroundUrls = [
   "https://chandra.harvard.edu/photo/2017/dark/dark_lg.jpg",
   "https://chandra.harvard.edu/photo/2009/gridge/gridge_lg.jpg",
   "https://chandra.harvard.edu/photo/2011/cdfs/cdfs_lg.jpg",
+  "https://i.pinimg.com/originals/41/41/60/414160638576cb93dbf6d70eae2d98fd.png",
+  "https://vapa.vn/wp-content/uploads/2022/12/hinh-nen-may-tinh-4k-thien-nhien-001.jpg",
+  "https://hapotravel.com/wp-content/uploads/2023/03/tong-hop-25-hinh-nen-thien-nhien-4k-dep-nhat-cho-desktop-va-mobile_25.jpg",
+  "https://c4.wallpaperflare.com/wallpaper/586/603/742/minimalism-4k-for-mac-desktop-wallpaper-preview.jpg",
+  "https://st.quantrimang.com/photos/image/2018/01/10/hinh-nen-4k-5.jpg",
+  "https://cdn.sforum.vn/sforum/wp-content/uploads/2023/06/tai-hinh-nen-dep-nhat-the-gioi-57.jpg"
 ];
 
 export const testBackgroundUrl = null;
